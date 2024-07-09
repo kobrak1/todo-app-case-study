4eac9aca6c32677d745bd8856f14d1bf9cfb573a
kobrak1 <burakkarhan657@gmail.com>
2024-07-09 11:13:02 +0300

    add: todo and user models were added

---
+++ api/models/todo.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose')
+
+const todoSchema = new mongoose.Schema({
+    user_id: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    text: {
+        type: String,
+        minLength: 1,
+        maxLength: [4000, 'Too many characters, max length is 4000'],
+        required: true
+    },
+    tags: [ String ],
+    image: {
+        data: Buffer,
+        contentType: String
+    },
+    file: {
+        data: Buffer,
+        contentType: String
+    },
+    updated_at: { type: Date, default: Date.now },
+    created_at: { type: Date, default: Date.now },
+})
+
+todoSchema.set('toJSON', {
+    transform: (doc, ret) => {
+        ret.id = ret._id.toString()
+        delete ret._id
+        delete ret.__v
+        return ret
+    }
+})
+
+const Todo = mongoose.model('Todo', todoSchema)
+module.exports = Todo
---
+++ api/models/user.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose')
+
+const userSchema = new mongoose.Schema({
+    name: { 
+        type: String, 
+        required: true 
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    passwordHash: {
+        type: String,
+        required: true
+    },
+    todos: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Todo'
+        }
+    ],
+    updated_at: { type: Date, default: Date.now },
+    created_at: { type: Date, default: Date.now },
+})
+
+todoSchema.set('toJSON', {
+    transform: (doc, ret) => {
+        ret.id = ret._id.toString()
+        delete ret._id
+        delete ret.__v
+        delete ret.passwordHash
+    }
+})
+
+const User = mongoose.model('User', userSchema)
+module.exports = User
